12999cb1f1a60269ebcf9ad52ba7f7b3a66bac84
yolandaaisha99 <ayishaolayda433@gmail.com>
2026-05-26 02:45:50 +0700

    fix: add missing Laravel service providers

---
+++ backend/app/Providers/AuthServiceProvider.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    protected $policies = [];
+    public function boot(): void {}
+}
