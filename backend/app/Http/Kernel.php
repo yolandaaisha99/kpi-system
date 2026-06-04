74ca85549a4bb2c36373941ffdff9314151fac73
fookinbarrie <your-email@example.com>
2026-06-04 11:01:12 +0700

    fix: ubah folder models jadi kapital & aktifkan CORS di Kernel

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -6,7 +6,9 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
 {
-    protected $middleware = [];
+    protected $middleware = [
+        \Illuminate\Http\Middleware\HandleCors::class,
+    ];
 
     protected $middlewareGroups = [
         'web' => [],
